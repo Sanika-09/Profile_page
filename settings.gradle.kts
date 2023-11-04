08481b89ea4fcd479f00c0048a829c42dc5a1ddf
Sanika-09 <114805270+Sanika-09@users.noreply.github.com>
2023-11-04 20:12:02 +0530

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Profile_page"
+include(":app")
+ 
